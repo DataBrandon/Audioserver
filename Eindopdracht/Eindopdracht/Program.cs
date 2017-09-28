ffd34c25c780af43bf5f26710b1badea837d3359
DeMoordMuis <bvostaden@hotmail.com>
2017-09-28 09:37:11 +0200

    Start Server
    primary loop begonnen

--- Eindopdracht/Eindopdracht/Program.cs
+++ Eindopdracht/Eindopdracht/Program.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
 
 namespace Eindopdracht
 {
     class Program
     {
+
+
+
+
+
         static void Main(string[] args)
         {
+            
+            IPAddress lookback = new IPAddress.TryParse("127.0.0.1");
+            TcpListener listener = new TcpListener(lookback, 6969);
+			
+            listener.Start();
+            while(true){
+                Console.WriteLine("Waiting for Connection");
+
+                TcpClient client = listener.AcceptTcpClient();
+                Thread t = new Thread();
+
+            }
+
+
+
         }
+
+
+
+
+
+
+
     }
 }
